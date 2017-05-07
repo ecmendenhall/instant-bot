3ba51431d1e173d6d549edca0470123605ac6a5a
Connor Mendenhall <ecmendenhall@gmail.com>
2017-05-06 20:06:06 -0400

    Rewire stuff. Filter tweets before lastRun.

---
+++ __mocks__/twit.js
@@ -0,0 +1,46 @@
+class MockTwit {
+
+  constructor(credentials) {
+    this.credentials = credentials;
+  }
+
+  post(path, options) {
+    this.postPath = path;
+    this.postOptions = options;
+    switch(path) {
+    case 'statuses/update':
+      return new Promise((resolve, reject) => {
+        resolve({id: 1});
+      });
+    default:
+      break;
+    }
+  }
+
+  get(path, options) {
+    this.getPath = path;
+    this.getOptions = options;
+    switch(path) {
+    case 'statuses/home_timeline':
+      return new Promise((resolve, reject) => {
+        resolve({data: [
+          {text: 'Some tweet text', created_at: 'Sat May 06 23:00:00 +0000 2017'},
+          {text: 'Some tweet text', created_at: 'Sat May 06 23:01:00 +0000 2017'},
+          {text: 'Some tweet text', created_at: 'Sat May 06 23:02:00 +0000 2017'}
+        ]});
+      });
+    case 'statuses/mentions_timeline':
+      return new Promise((resolve, reject) => {
+        resolve({data: [
+          {text: 'Some mention text', created_at: 'Sat May 06 23:00:00 +0000 2017'},
+          {text: 'Some mention text', created_at: 'Sat May 06 23:01:00 +0000 2017'},
+          {text: 'Some mention text', created_at: 'Sat May 06 23:02:00 +0000 2017'}
+        ]});
+      });
+    default:
+      break;
+    }
+  }
+}
+
+module.exports = MockTwit;
