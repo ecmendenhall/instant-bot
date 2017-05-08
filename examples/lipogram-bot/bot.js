f91bf618828cabc43eabac49deefa89863923246
Connor Mendenhall <ecmendenhall@gmail.com>
2017-05-07 22:13:41 -0400

    Add comments to example.

--- examples/lipogram-bot/bot.js
+++ examples/lipogram-bot/bot.js
@@ -1,18 +1,25 @@
 const instantBot = require('instant-bot');
 
+// This Twitter bot retweets mentions and tweets that it sees in its home timeline
+// if they don't contain the letter "e"
+
 module.exports.run = () => {
 
   instantBot().then((bot) => {
-    bot.messages.filter(
-      m => !(m.text.includes('e') || m.text.includes('E'))
-    ).map(
-      message => bot.service.client.post('statuses/retweet/:id', {id: message.id_str})
-    );
+
+    // Access new messages since last time the bot ran
+    bot.messages.filter((m) => {
+       // Filter messages with "e"
+      return !(m.text.includes('e') || m.text.includes('E'))
+    }).map((message) => {
+       // Use the Twit client to create a retweet directly
+       bot.client.post('statuses/retweet/:id', {id: message.id_str})
+    });
 
     bot.mentions.filter(
       m => !(m.text.includes('e') || m.text.includes('E'))
     ).map(
-      message => bot.service.client.post('statuses/retweet/:id', {id: message.id_str})
+      message => bot.client.post('statuses/retweet/:id', {id: message.id_str})
     );
   });
 
